2b9f21946019e85dbcc2dce0187a6fbec3dc160e
Bjorn De Craemer <bjorn.decraemer@yahoo.com>
2019-01-27 01:30:09 +0100

    Added Sleuth Distributed Tracing

--- src/main/java/com/bjornspetprojects/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ src/main/java/com/bjornspetprojects/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -3,6 +3,8 @@ package com.bjornspetprojects.currencyexchangeservice.controllers;
 import com.bjornspetprojects.currencyexchangeservice.beans.ExchangeValue;
 import com.bjornspetprojects.currencyexchangeservice.exceptions.ExchangeValueNotExistingException;
 import com.bjornspetprojects.currencyexchangeservice.services.ExchangeValueService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +16,11 @@ import java.util.Objects;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     private final Environment environment;
     private ExchangeValueService exchangeValueService;
+
 
     public CurrencyExchangeController(Environment environment, ExchangeValueService exchangeValueService) {
         this.environment = environment;
@@ -26,6 +31,7 @@ public class CurrencyExchangeController {
     public ResponseEntity<ExchangeValue> retrieveExchangeValue(@PathVariable String from, @PathVariable String to) throws ExchangeValueNotExistingException {
         ExchangeValue exchangeValue = exchangeValueService.findByFromAndToIgnoreCase(from,to);
         exchangeValue.setPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port"))));
+        logger.info("{}", exchangeValue);
         return ResponseEntity.ok(exchangeValue);
     }
 }
